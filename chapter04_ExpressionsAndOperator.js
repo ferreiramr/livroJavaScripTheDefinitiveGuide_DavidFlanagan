e5ea2f6079dbf9f5fe39a930eff39ae436943ff9
Marcos Ferreira <ferreira.mr@pm.me>
2022-04-17 18:42:03 -0300

    Dia 006

---
+++ chapter04_ExpressionsAndOperator.js
@@ -0,0 +1,19 @@
+// CHAPTER 4 - EXPRESSIONS AND OPERATORS
+
+// PRIMARY EXPRESSIONS
+
+1.23 // A number literal
+"hello" // A string literal
+/pattern/ // A regular expression literal
+
+true // Evaluates to the boolen true value
+false // Evaluates to the boolena false value
+null // Evauatess  to the null value
+this // Evaluates to the "current" object
+
+i // Evakyates to the value of the variable i.
+sum // Evaluates to the value of the variable sum.
+undefined // The value of the "undefined" property of the global object
+
+[] // An empyt array: no expression inside bracktets means no elements
+[1+2, 3+4] // A 2-element array. Fist elements is 3, second is 7
